e748e902767fad82645d9512ffe4ccb7a6d25a67
Ian McCall <imccall@tribuneinteractive.com>
2019-09-10 09:58:36 -0500

    Implement modular exponentiation. And upgrade to u64

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use num_traits::pow;
 use rand::Rng;
 use std::io;
 
@@ -7,7 +6,7 @@ fn main() {
         println!("Enter number to check");
         let mut n = String::new();
         io::stdin().read_line(&mut n).expect("Faild to read line");
-        let n: u32 = match n.trim().parse() {
+        let n: u64 = match n.trim().parse() {
             Ok(num) => num,
             Err(_) => continue
         };
@@ -19,7 +18,23 @@ fn main() {
     }
 }
 
-fn prime_check(n: u32, mut k: u32) -> bool {
+fn modpow(mut base: u64, mut exponent: u64, modulus: u64) -> u64 {
+    if modulus == 1 {
+        return 0
+    }
+    let mut result: u64 = 1;
+    base = base % modulus;
+    while exponent > 0 {
+        if exponent % 2 == 1 {
+            result = (result * base) % modulus;
+        }
+        exponent = exponent >> 1;
+        base = (base * base) % modulus;
+    }
+    return result
+}
+
+fn prime_check(n: u64, mut k: u32) -> bool {
     if n == 1 || n == 3 {
         return true;
     } else if n % 2 == 0 {
@@ -27,11 +42,11 @@ fn prime_check(n: u32, mut k: u32) -> bool {
     }
     let mut rng = rand::thread_rng();
     while k > 0 {
-        let a = rng.gen_range(2, n - 2);
+        let a: u64 = rng.gen_range(2, n - 2);
         println!("n: {}; k: {}; a: {}", n, k, a);
-        let x = pow(a,n-1);
-        println!("(a^(n-1)): {}", x);
-        if (x % n) != 1 {
+        let x = modpow(a,n-1, n);
+        println!("(a^(n-1)) % n: {}", x);
+        if x != 1 {
             return false
         }
         k = k - 1;
