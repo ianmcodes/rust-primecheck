3e8fd88e77ffe9d166f705530d9a065a4fb3dcbd
Ian McCall <imccall@tribuneinteractive.com>
2019-09-13 17:10:56 -0500

    Documentation

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wasm-pack build --release -t web
+if [ $? -ne 0 ]; then
+  echo "Build failed!"
+  exit 1
+fi
+cp ./pkg/prime_check* ./doc/pkg/
+
+cargo doc --no-deps
+cp -r ./target/doc ./doc/doc
---
+++ doc/doc/search-index.js
@@ -0,0 +1,5 @@
+var N=null,E="",T="t",U="u",searchIndex={};
+var R=["prime_check"];
+
+searchIndex["prime_check"]={"doc":E,"i":[[5,"rng_gen_range",R[0],"Generates a random number in the given range.",N,[[["u64"]],["u64"]]],[5,"modpow",E,"Implementation of modular exponentiation for u64. Exposed…",N,[[["u64"]],["u64"]]],[5,R[0],E,"Implementation of Fermat Prime Test. Returns true if the…",N,[[["u32"],["u64"]],["bool"]]]],"p":[]};
+initSearch(searchIndex);addSearchOptions(searchIndex);
